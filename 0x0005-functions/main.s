d1ef73b54af780b6674b1f4d522790d9079a3031
Kevin Thomas <kevin@mytechnotalent.com>
2023-08-30 12:16:35 -0400

    Added files for Chapter 11

---
+++ 0x0005-functions/main.s
@@ -0,0 +1,279 @@
+/**
+ * FILE: main.s
+ *
+ * DESCRIPTION:
+ * This file contains the assembly code for functions
+ * utilizing the STM32F401CC6 microcontroller.
+ *
+ * AUTHOR: Kevin Thomas
+ * CREATION DATE: July 22, 2023
+ * UPDATE Date: July 22, 2023
+ *
+ * ASSEMBLE AND LINK w/ SYMBOLS:
+ * 1. arm-none-eabi-as -g main.s -o main.o
+ * 2. arm-none-eabi-ld main.o -o main.elf -T stm32f401ccux.ld
+ * 3. openocd -f interface/stlink-v2.cfg -f target/stm32f4x.cfg -c "program main.elf verify reset exit"
+ * ASSEMBLE AND LINK w/o SYMBOLS:
+ * 1. arm-none-eabi-as -g main.s -o main.o
+ * 2. arm-none-eabi-ld main.o -o main.elf -T stm32f401ccux.ld
+ * 3. arm-none-eabi-objcopy -O binary --strip-all main.elf main.bin
+ * 3. openocd -f interface/stlink-v2.cfg -f target/stm32f4x.cfg -c "program main.bin verify reset exit"
+ * DEBUG w/ SYMBOLS:
+ * 1. openocd -f interface/stlink-v2.cfg -f target/stm32f4x.cfg
+ * 2. arm-none-eabi-gdb main.elf
+ * 3. target remote :3333
+ * 4. monitor reset halt
+ * 5. l
+ * DEBUG w/o SYMBOLS:
+ * 1. openocd -f interface/stlink-v2.cfg -f target/stm32f4x.cfg
+ * 2. arm-none-eabi-gdb main.bin
+ * 3. target remote :3333
+ * 4. monitor reset halt
+ * 5. x/8i $pc
+ */
+
+.syntax unified
+.cpu cortex-m4
+.thumb
+
+/**
+ * Provide weak aliases for each Exception handler to the Default_Handler.
+ * As they are weak aliases, any function with the same name will override
+ * this definition.
+ */
+.macro weak name
+  .global \name
+  .weak \name
+  .thumb_set \name, Default_Handler
+  .word \name
+.endm
+
+/**
+ * The STM32F401CCUx vector table.  Note that the proper constructs
+ * must be placed on this to ensure that it ends up at physical address
+ * 0x0000.0000.
+ */
+.global isr_vector 
+.section .isr_vector, "a"
+.type isr_vector, %object
+isr_vector:
+  .word _estack 
+  .word Reset_Handler
+   weak NMI_Handler
+   weak HardFault_Handler
+   weak MemManage_Handler
+   weak BusFault_Handler
+   weak UsageFault_Handler
+  .word 0
+  .word 0
+  .word 0
+  .word 0
+   weak SVC_Handler
+   weak DebugMon_Handler
+  .word 0
+   weak PendSV_Handler
+   weak SysTick_Handler
+  .word 0
+   weak EXTI16_PVD_IRQHandler                              // EXTI Line 16 interrupt /PVD through EXTI line detection interrupt
+   weak TAMP_STAMP_IRQHandler                              // Tamper and TimeStamp interrupts through the EXTI line
+   weak EXTI22_RTC_WKUP_IRQHandler                         // EXTI Line 22 interrupt /RTC Wakeup interrupt through the EXTI line
+   weak FLASH_IRQHandler                                   // FLASH global interrupt
+   weak RCC_IRQHandler                                     // RCC global interrupt
+   weak EXTI0_IRQHandler                                   // EXTI Line0 interrupt
+   weak EXTI1_IRQHandler                                   // EXTI Line1 interrupt
+   weak EXTI2_IRQHandler                                   // EXTI Line2 interrupt
+   weak EXTI3_IRQHandler                                   // EXTI Line3 interrupt
+   weak EXTI4_IRQHandler                                   // EXTI Line4 interrupt
+   weak DMA1_Stream0_IRQHandler                            // DMA1 Stream0 global interrupt
+   weak DMA1_Stream1_IRQHandler                            // DMA1 Stream1 global interrupt
+   weak DMA1_Stream2_IRQHandler                            // DMA1 Stream2 global interrupt
+   weak DMA1_Stream3_IRQHandler                            // DMA1 Stream3 global interrupt
+   weak DMA1_Stream4_IRQHandler                            // DMA1 Stream4 global interrupt
+   weak DMA1_Stream5_IRQHandler                            // DMA1 Stream5 global interrupt
+   weak DMA1_Stream6_IRQHandler                            // DMA1 Stream6 global interrupt
+   weak ADC_IRQHandler                                     // ADC1 global interrupt
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+   weak EXTI9_5_IRQHandler                                 // EXTI Line[9:5] interrupts
+   weak TIM1_BRK_TIM9_IRQHandle                            // TIM1 Break interrupt and TIM9 global interrupt
+   weak TIM1_UP_TIM10_IRQHandler                           // TIM1 Update interrupt and TIM10 global interrupt
+   weak TIM1_TRG_COM_TIM11_IRQHandler                      // TIM1 Trigger and Commutation interrupts and TIM11 global interrupt
+   weak TIM1_CC_IRQHandler                                 // TIM1 Capture Compare interrupt
+   weak TIM2_IRQHandler                                    // TIM2 global interrupt
+   weak TIM3_IRQHandler                                    // TIM3 global interrupt
+   weak TIM4_IRQHandler                                    // TIM4 global interrupt
+   weak I2C1_EV_IRQHandler                                 // I2C1 event interrupt
+   weak I2C1_ER_IRQHandler                                 // I2C1 error interrupt
+   weak I2C2_EV_IRQHandler                                 // I2C2 event interrupt
+   weak I2C2_ER_IRQHandler                                 // I2C2 error interrupt
+   weak SPI1_IRQHandler                                    // SPI1 global interrupt
+   weak SPI2_IRQHandler                                    // SPI2 global interrupt
+   weak USART1_IRQHandler                                  // USART1 global interrupt
+   weak USART2_IRQHandler                                  // USART2 global interrupt
+  .word 0                                                  // Reserved
+   weak EXTI15_10_IRQHandler                               // EXTI Line[15:10] interrupts
+   weak EXTI17_RTC_Alarm_IRQHandler                        // EXTI Line 17 interrupt / RTC Alarms (A and B) through EXTI line interrupt
+   weak EXTI18_OTG_FS_WKUP_IRQHandler                      // EXTI Line 18 interrupt / USBUSB OTG FS Wakeup through EXTI line interrupt
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+   weak DMA1_Stream7_IRQHandler                            // DMA1 Stream7 global interrupt
+  .word 0                                                  // Reserved
+   weak SDIO_IRQHandler                                    // SDIO global interrupt
+   weak TIM5_IRQHandler                                    // TIM5 global interrupt
+   weak SPI3_IRQHandler                                    // SPI3 global interrupt
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+   weak DMA2_Stream0_IRQHandler                            // DMA2 Stream0 global interrupt
+   weak DMA2_Stream1_IRQHandler                            // DMA2 Stream1 global interrupt
+   weak DMA2_Stream2_IRQHandler                            // DMA2 Stream2 global interrupt
+   weak DMA2_Stream3_IRQHandler                            // DMA2 Stream3 global interrupt
+   weak DMA2_Stream4_IRQHandler                            // DMA2 Stream4 global interrupt
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+   weak OTG_FS_IRQHandler                                  // USB On The Go FS global interrupt
+   weak DMA2_Stream5_IRQHandler                            // DMA2 Stream5 global interrupt
+   weak DMA2_Stream6_IRQHandler                            // DMA2 Stream6 global interrupt
+   weak DMA2_Stream7_IRQHandler                            // DMA2 Stream7 global interrupt
+   weak USART6_IRQHandler                                  // USART6 global interrupt
+   weak I2C3_EV_IRQHandler                                 // I2C3 event interrupt
+   weak I2C3_ER_IRQHandler                                 // I2C3 error interrupt
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+  .word 0                                                  // Reserved
+   weak SPI4_IRQHandler                                    // SPI4 global interrupt
+
+.section .text
+
+/**
+ * @brief  This code is called when processor starts execution.
+ *
+ *         This is the code that gets called when the processor first
+ *         starts execution following a reset event. Only the absolutely
+ *         necessary set is performed, after which the application
+ *         supplied main() routine is called.
+ * 
+ * @param  None
+ * @retval None
+ */
+.type Reset_Handler, %function
+.global Reset_Handler
+Reset_Handler:
+  LDR  R0, =_estack
+  MOV  SP, R0
+  BL   __start
+
+/**
+ * @brief  This code is called when the processor receives and unexpected interrupt.
+ *
+ *         This is the code that gets called when the processor receives an
+ *         unexpected interrupt.  This simply enters an infinite loop, preserving
+ *         the system state for examination by a debugger.
+ *
+ * @param  None
+ * @retval None
+ */
+.type Default_Handler, %function
+.global Default_Handler
+Default_Handler:
+  BKPT
+  B.N  Default_Handler
+
+/**
+ * @brief  Entry point for initialization and setup of specific functions.
+ *
+ *         This function is the entry point for initializing and setting up specific functions.
+ *         It calls other functions to enable certain features and then enters a loop for further execution.
+ *
+ * @param  None
+ * @retval None
+ */
+.type __start, %function
+__start:
+  BL   logical_shift_left                                  // call function
+
+  BL   logical_shift_right                                 // call function
+
+  BL   arithmetic_shift_right                              // call function
+
+  BL   rotate_right                                        // call function
+
+  B    .                                                   // branch infinite loop
+
+/**
+ * @brief  Logical shift left function to demonstrate an LSL instruction.
+ *
+ *         This function demonstrates a LSL instruction.
+ *
+ * @param  None
+ * @retval None
+ */
+.type logical_shift_left, %function
+logical_shift_left:
+  PUSH {LR}                                                // push return address onto stack
+  LDR  R0, =0b00000000000000000000000000000001             // move 0x01 into R0
+  LSL  R0, #0x01                                           // logical shift left 0x01, multiply by 2, LSB gets 0
+  POP  {LR}                                                // pop return address from stack
+  BX   LR                                                  // return to caller
+
+/**
+ * @brief  Logical shift right function to demonstrate an LSR instruction.
+ *
+ *         This function demonstrates a LSR instruction.
+ *
+ * @param  None
+ * @retval None
+ */
+.type logical_shift_right, %function
+logical_shift_right:
+  PUSH {LR}                                                // push return address onto stack                                     
+  LDR  R0, =0b00000000000000000000000000000010             // move 0x02 into R0
+  LSR  R0, #0x01                                           // logical shift right 0x01, integer divide by 2, MSB gets 0
+  POP  {LR}                                                // pop return address from stack
+  BX   LR                                                  // return to caller
+
+/**
+ * @brief  Arithmetic shift right function to demonstrate an ASR instruction.
+ *
+ *         This function demonstrates an ASR instruction.
+ *
+ * @param  None
+ * @retval None
+ */
+arithmetic_shift_right:
+  PUSH {LR}                                                // push return address onto stack 
+  LDR  R0, =0b10000000000000000000000000000010             // move 0x80000002 into R0
+  ASR  R0, #0x01                                           // arithmetic shift right 0x01, integer divide by 2, MSB preserves sign
+  POP  {LR}                                                // pop return address from stack
+  BX   LR                                                  // return to caller
+
+/**
+ * @brief  Rotate right function to demonstrate an ASR instruction.
+ *
+ *         This function demonstrates an ROR instruction.
+ *
+ * @param  None
+ * @retval None
+ */
+rotate_right:
+  PUSH {LR}                                                // push return address onto stack  
+  LDR  R0, =0b00000000000000000000000000000010             // move 0x02 into R0
+  ROR  R0, #0x01                                           // rotate right 0x01, multiply by 2, LSB gets original MSB value
+  POP  {LR}                                                // pop return address from stack
+  BX   LR                                                  // return to caller
